1bce1ac4d7eb3fb3e2919698e7bd63c1dc5d210c
2400090161GNVT <2400090161@kluniversity.in>
2025-12-24 09:51:37 +0530

    first commit

---
+++ arrays.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,sum=0;
+	scanf("%d",&n);
+	int a[n]={1,2,3,4};
+	for(int i=0;i<n;i++)
+	{
+	
+	scanf("%d",&a[i]);
+	for(int i=0;i<n;i++){
+	
+	sum=sum+a[i];
+}
+	printf("%d",sum);
+	return 0;
+}
+*
